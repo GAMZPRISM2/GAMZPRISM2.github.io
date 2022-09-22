97906fae28b754642775e196d02d0dd35a7a693c
GAMZPRISM2 <109406122+GAMZPRISM2@users.noreply.github.com>
2022-09-22 08:52:24 +0800

    Add files via upload

---
+++ products.js
@@ -0,0 +1,27 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+    123: {
+      name : "Pepperoni",
+      desc : "Pepperoni ona  tomato base",
+      img : "dummy-pdt-b.jpg",
+      price : 20
+    },
+    124: {
+      name : "MokBook Rookie",
+      desc : "Unsatiable its possession nor off.",
+      img : "dummy-pdt-b.jpg",
+      price : 1247
+    },
+    125: {
+      name : "iPong Max",
+      desc : "All difficulty unreserved the solicitude.",
+      img : "dummy-pdt-a.jpg",
+      price : 675
+    },
+    126: {
+      name : "iTab Pok",
+      desc : "Had judgment out property the supplied. ",
+      img : "dummy-pdt-a.jpg",
+      price : 842
+    }
+  };
